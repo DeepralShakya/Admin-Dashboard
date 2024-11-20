37074a9f1157004ecc52da661726cba51df924a8
DeepralShakya <shakyadeepral44@gmail.com>
2024-11-20 16:13:30 +0545

    Shadcn data table Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 'use client'
 
 import DashboardCard from "@/components/dashboard/dashboardCard";
+import PostTable from "@/components/posts/PostTable";
 import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
-      <DashboardCard title="Posts" count={200} icon={<Newspaper className="text-orange-500" size={72}/>}/>
-      <DashboardCard title="Categories" count={30} icon={<Folder className="text-orange-500" size={72}/>}/>
-      <DashboardCard title="Users" count={1000} icon={<User className="text-orange-500" size={72}/>}/>
-      <DashboardCard title="Comments" count={1500} icon={<MessageCircle className="text-orange-500" size={72}/>}/>
+    <div>
+      <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
+        <DashboardCard title="Posts" count={200} icon={<Newspaper className="text-orange-500" size={72}/>}/>
+        <DashboardCard title="Categories" count={30} icon={<Folder className="text-orange-500" size={72}/>}/>
+        <DashboardCard title="Users" count={1000} icon={<User className="text-orange-500" size={72}/>}/>
+        <DashboardCard title="Comments" count={1500} icon={<MessageCircle className="text-orange-500" size={72}/>}/>
+      </div>
+      <PostTable title="Latest Posts"/>
     </div>
   );
 }
